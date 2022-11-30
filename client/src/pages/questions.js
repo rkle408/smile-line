9b023729a31087d093d1105ae0b0f80681c8f4bc
Rosebud <rkle408@gmail.com>
2022-11-29 18:14:45 -0800

    added FAQs page

---
+++ client/src/pages/questions.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Questions () {
+
+    return (
+        <section>
+            <h1>Frequently Asked Dental Questions</h1>
+
+            <div>
+                <h2>Why should I floss??</h2>
+                <p>Brushing will miss the areas in between your teeth! And it's definitely not going to get out that piece of garlic lodged into your gums!<br/>
+                    It'll help to remove plaque/build up and bacteria that you can't see or feel. That'll help to reduce bleeding gums!<br/><br/>
+                    <a href="https://www.youtube.com/watch?v=CBd_2Dk0Svw">Click here to see a video on proper flossing!</a>
+                </p>
+
+                <h2>Why should I use string floss? It's so hard, and I'd rather use a water flosser or a floss pick...</h2>
+                <p>If you are physically able to use string floss, it's the best option because it can curve around your teeth, and actually remove the plaque!<br/>
+                A different type of floss is a great ADDITION to string flossing, but is usually insufficient on its own because it could just move the plaque lower into your gums, or can even cause damage to them if used incorrectly or with too much force!</p>
+
+                <h2></h2>
+                <p></p>
+
+                <h2></h2>
+                <p></p>
+
+            </div>
+
+        </section>
+    )
+};
+
+export default Questions;
