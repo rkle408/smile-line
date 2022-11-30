95cad84629e27cdf43120c597942072ce17ee559
Rosebud <rkle408@gmail.com>
2022-11-29 18:24:47 -0800

    added questions and answers to FAQ page

--- client/src/pages/questions.js
+++ client/src/pages/questions.js
@@ -9,7 +9,7 @@ function Questions () {
             <div>
                 <h2>Why should I floss??</h2>
                 <p>Brushing will miss the areas in between your teeth! And it's definitely not going to get out that piece of garlic lodged into your gums!<br/>
-                    It'll help to remove plaque/build up and bacteria that you can't see or feel. That'll help to reduce bleeding gums!<br/><br/>
+                It'll help to remove plaque/build up and bacteria that you can't see or feel. Removing the plaque with floss will help to remove the bacteria that causes swollen and bleeding gums!<br/><br/>
                     <a href="https://www.youtube.com/watch?v=CBd_2Dk0Svw">Click here to see a video on proper flossing!</a>
                 </p>
 
@@ -17,8 +17,8 @@ function Questions () {
                 <p>If you are physically able to use string floss, it's the best option because it can curve around your teeth, and actually remove the plaque!<br/>
                 A different type of floss is a great ADDITION to string flossing, but is usually insufficient on its own because it could just move the plaque lower into your gums, or can even cause damage to them if used incorrectly or with too much force!</p>
 
-                <h2></h2>
-                <p></p>
+                <h2>Why do I need to see a dentist twice a year?</h2>
+                <p>We need to check all aspects of your teeth, especially the surfaces you can't see and that are hard to reach, to make sure they're healthy or if they're not, to catch it before it becomes a bigger problem! Dealing with a small cavity is always easier and cheaper to deal with than a crown, root canal, or pulling the tooth out!</p>
 
                 <h2></h2>
                 <p></p>
