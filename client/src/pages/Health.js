ed66aa6fb9e289b3ba68c7561d24b12928179e00
Vivian Chen <vivian.chen1193@gmail.com>
2022-11-29 18:44:18 -0800

    added more medical info in health hist

--- client/src/pages/Health.js
+++ client/src/pages/Health.js
@@ -61,6 +61,81 @@ const Health = () => {
                 <label> Date: <input id="tjr" type="text"/> </label>
                 <label> If yes, have you had any complications? <input id="tjr" type="text"/> </label>
             </div>
+            <div>
+                <div>
+                Are you taking or scheduled to begin taking an antiresorptive agent (like Fosamax®, Actonel®, Atelvia, Boniva®, Reclast, Prolia) for osteoporosis or Paget's disease?...
+                </div>
+                <label htmlFor='bis'>
+                <input id="bis" type='checkbox' />
+                Yes
+                </label>
+                <label htmlFor='bis'>
+                <input id="bis" type='checkbox' />
+                No
+                </label>
+            </div>
+            <div>
+                <div>
+                    Please select the box to indicate if you HAVE had any of the following diseases or problems.
+                </div>
+                <label htmlFor='CVD'>
+                <input id="CVD" type='checkbox' />
+                    Cardiovascular Disease
+                </label>
+                <label htmlFor='angina'>
+                <input id="angina" type='checkbox' />
+                    Angina
+                </label>
+                <label htmlFor='CHF'>
+                <input id="CHF" type='checkbox' />
+                    Congestive Heart Failure
+                </label>
+                <label htmlFor='DHV'>
+                <input id="DHV" type='checkbox' />
+                    Damaged Heart Valves
+                </label>
+                <label htmlFor='HA'>
+                <input id="HA" type='checkbox' />
+                    Heart Attack
+                </label>
+                <label htmlFor='HBP'>
+                <input id="HBP" type='checkbox' />
+                    High Blood Pressure
+                </label>
+                <label htmlFor='OCHD'>
+                <input id="OCHD" type='checkbox' />
+                    Other Congenital Heart Defects
+                </label>
+                <label htmlFor='aids'>
+                <input id="aids" type='checkbox' />
+                    AIDS or HIV infection
+                </label>
+                <label htmlFor='asthma'>
+                <input id="asthma" type='checkbox' />
+                    Asthma
+                </label>
+                <label htmlFor='stroke'>
+                <input id="stroke" type='checkbox' />
+                    Stroke
+                </label>
+                <label htmlFor='gerd'>
+                <input id="gerd" type='checkbox' />
+                    GERD
+                </label>
+                <label htmlFor='seizures'>
+                <input id="seizures" type='checkbox' />
+                    Seizures
+                </label>
+                <label htmlFor='epilepsy'>
+                <input id="epilepsy" type='checkbox' />
+                    Epilepsy
+                </label>
+                <label htmlFor='OT'>
+                <input id="OT" type='checkbox' />
+                    Organ Transplants Specify: 
+                <input id= "OT" type="text" />
+                </label>
+            </div>
         </form>
     </div>
   )
