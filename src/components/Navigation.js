ceb327772cc4f8605b638bce03cb0bc63269c398
Vivian Chen <vivian.chen1193@gmail.com>
2022-12-07 15:02:38 -0800

    hamburger navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom"; //removed useNavigate
 import React from 'react'; //removed useCallBack
 // import { useStytchSession, useStytch } from '@stytch/stytch-react';
+import { Navbar, Nav} from 'react-bootstrap';
+
 
 
 import './Navigation.css';
@@ -19,21 +21,27 @@ import './Navigation.css';
 
     return ( 
         <div>
+            <Navbar collapseOnSelect expand='lg' className='nav'>
             <div className="brand">
                 <a href='/' className="smile"><h1>SmileLine</h1></a>
             </div>
-            <nav style={{display: "flex", flexWrap: "wrap", gap: "50px"}}>
-                <NavLink to='/questions' className="navlink">Your Questions</NavLink>
-                <NavLink to='/terms' className="navlink">Translate: Dental Terms</NavLink>
-                <NavLink to='/find' className="navlink">Find A Dentist</NavLink>
-                <NavLink to='/health' className="navlink">Health History Form</NavLink>
-                <NavLink to='/HIPAA' className="navlink">Consent Forms</NavLink>
-                <NavLink to='/fees' className="navlink">Dental Fees</NavLink>
-                <NavLink to='/contact' className="navlink">Contact SmileLine</NavLink>
-                {/* <div> <NavLink to='/login'>Login</NavLink></div> */}
-                {/* <div className="button"> <button onClick={handleLogout}>Logout</button> </div> */}
-            
-            </nav>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id='responsive-navbar-nav' style={{justifyContent: 'space-around'}}>
+                <Nav style={{gap: '20px'}}>
+                    <NavLink to='/questions' className="navlink">Your Questions</NavLink>
+                    <NavLink to='/terms' className="navlink">Translate: Dental Terms</NavLink>
+                    <NavLink to='/find' className="navlink">Find A Dentist</NavLink>
+                    <NavLink to='/health' className="navlink">Health History Form</NavLink>
+                    <NavLink to='/HIPAA' className="navlink">Consent Forms</NavLink>
+                    <NavLink to='/fees' className="navlink">Dental Fees</NavLink>
+                    <NavLink to='/contact' className="navlink">Contact SmileLine</NavLink>
+                    {/* <div> <NavLink to='/login'>Login</NavLink></div> */}
+                    {/* <div className="button"> <button onClick={handleLogout}>Logout</button> </div> */}
+                
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 };
