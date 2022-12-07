792d79b985b2b8d75b33e669922f0c04863bfaf3
Rosebud <rkle408@gmail.com>
2022-12-06 23:36:21 -0800

    added Terms page

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -8,7 +8,24 @@ const Terms = () => {
         <div className="terms">
             <h1 className="title">What's that word you're using, doc?</h1>
             <Card>
-                <Card.Header>Caries</Card.Header>
+                <Card.Header  as="h5">Caries</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Caries is just another word for cavities! If you know dental Spanish, then this should be the same!
+                            {' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+            <br/>
+            
+            <Card>
+                <Card.Header as="h5">Caries</Card.Header>
                 <Card.Body>
                     <blockquote className="blockquote mb-0">
                         <p>
